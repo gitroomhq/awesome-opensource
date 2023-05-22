3f423635c46fe78fe761482aeaa0d7aabd696e22
Nevo David <nevo@notu.co>
2023-05-23 01:39:22 +0700

    feat: js

---
+++ index.js
@@ -0,0 +1,5 @@
+(() => {
+	console.log('welcome to github20k');
+	console.log('feel free to add your library at: libraries.github20k.com');
+	console.log('feel free to contact me if you need any help at nevo@github20k.com');
+})();
