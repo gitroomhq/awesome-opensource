88f604278ab408f814c1811925590085c48213b2
Nevo David <100117126+nevo-david@users.noreply.github.com>
2023-05-25 13:55:52 +0700

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-(() => {
-	console.log('welcome to github20k');
-	console.log('feel free to add your library at: libraries.github20k.com');
-	console.log('feel free to contact me if you need any help at nevo@github20k.com');
-})();
